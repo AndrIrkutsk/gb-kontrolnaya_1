5dcd6e17e99e68126fb91dfe2d29a5dbb98e3f2f
Andrey Zimin <andrej.zimin.1984@mail.ru>
2024-04-09 17:18:57 +0800

    Add C_sharp directory and task

---
+++ C_sharp/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из 
+// строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с 
+// клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться 
+// коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] array = {"Hello", "2", "world", ":-)"};
+// string[] array = {"1234", "1567", "-2", "computer science"};
+// string[] array = {"Russia", "Denmark", "Kazan"};
+
+int GetCountRelevantElm(string[] array) {
+  int count = 0;
+
+  for(int i = 0; i < array.Length; i++) {
+    if(array[i].Length <= 3) count++;
+  }
+
+  return count;
+}
+
+void PrintArray(string[] array) {
+  foreach (var item in array)
+  {
+    Console.WriteLine($"{item} ");
+  }
+}
+
+void CreateNewArray(string[] array) {
+  int count = GetCountRelevantElm(array);
+  string[] newArr = new string[count];
+  count = 0;
+
+  for(int i = 0; i < array.Length; i++) {
+    if(array[i].Length <= 3) {
+      newArr[count] = array[i];
+      count++;
+    }
+  }
+
+  PrintArray(newArr);
+}
+
+CreateNewArray(array);
